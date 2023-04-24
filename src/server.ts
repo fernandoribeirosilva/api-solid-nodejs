39fae664146ec36fa8440b1a177b241baba9c1b6
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-04-23 21:18:47 -0300

    configurando a base do projeto

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import { app } from "./app";
+import { app } from './app'
+import { env } from './env'
 
-
-app.listen({
-  host: '0.0.0.0', // vai fazer com que a aplicação ficar acessível a outras aplicações frontEnd
-  port: 3333
-})
+app
+  .listen({
+    host: '0.0.0.0', // vai fazer com que a aplicação ficar acessível a outras aplicações frontEnd
+    port: env.PORT,
+  })
   .then(() => {
     console.log('🚀 HTTP Server running!')
   })

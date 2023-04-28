c51bad45b4073f746b90859ab411262068740985
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-04-28 19:10:11 -0300

    Repository pattern

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-user-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -22,11 +23,11 @@ export async function registerUseCase({
 
   if (userWithSameEmail) throw new Error('E-mail already exists!')
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
   })
 }

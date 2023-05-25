8ed5a328d3f355fb5c92e187b3de39fb3b9d4d01
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-05-25 06:03:21 -0300

    feat: criado rota privada /me

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,13 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from './controller/authenticate-controller'
 import { profile } from './controller/profile'
 import { register } from './controller/register-controller'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
 
-  app.get('/me', profile)
+  // Authenticated
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }

a3e4892b0f2d818b8abb056aa08586ceaa964e2b
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-05-26 08:37:58 -0300

    refactor: separando os arquivos do gyms

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,32 @@
+import { makeCreateGymsUseCase } from '@/use-cases/factories/make-create-gym-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string().nullable(),
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { title, description, phone, latitude, longitude } =
+    createBodySchema.parse(request.body)
+
+  const createGymsUseCase = makeCreateGymsUseCase()
+
+  await createGymsUseCase.execute({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  })
+
+  return reply.code(201).send()
+}

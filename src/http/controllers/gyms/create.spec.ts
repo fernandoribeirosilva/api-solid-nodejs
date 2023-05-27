d91f706ade3a91ab55d05fa0a2da3990d1d0b02b
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-05-27 08:08:57 -0300

    test: criado teste para validar a criação de uma academia

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready() // vai esperar a aplicação subir
+  })
+
+  afterAll(async () => {
+    await app.close() // vai aguardar a aplicação fechar
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Javascript Gyms',
+        description: 'Some description',
+        phone: '99999999999',
+        latitude: -13.2248733,
+        longitude: -49.5076427,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})

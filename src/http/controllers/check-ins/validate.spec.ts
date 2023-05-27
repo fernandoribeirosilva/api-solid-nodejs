b5050a5f513d618ff65fd8a4468afe6a77cfa3e6
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-05-27 10:07:14 -0300

    test: criado teste para validar o check-ins

---
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -0,0 +1,51 @@
+import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('validate Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready() // vai esperar a aplicação subir
+  })
+
+  afterAll(async () => {
+    await app.close() // vai aguardar a aplicação fechar
+  })
+
+  it('should be able to validate a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const user = await prisma.user.findFirstOrThrow()
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Javascript Gym',
+        latitude: -13.2248733,
+        longitude: -49.5076427,
+      },
+    })
+
+    let checkIn = await prisma.checkIn.create({
+      data: {
+        gym_Id: gym.id,
+        user_Id: user.id,
+      },
+    })
+
+    const response = await request(app.server)
+      .patch(`/check-ins/${checkIn.id}/validate`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(204)
+
+    checkIn = await prisma.checkIn.findUniqueOrThrow({
+      where: {
+        id: checkIn.id,
+      },
+    })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+  })
+})

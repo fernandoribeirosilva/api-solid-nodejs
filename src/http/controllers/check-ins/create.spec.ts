11af12f596c1a5ad7e2b0f0fd874866936c8ff2a
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-05-27 10:01:43 -0300

    test: criando teste para validar a criação de um usuário.

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,37 @@
+import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready() // vai esperar a aplicação subir
+  })
+
+  afterAll(async () => {
+    await app.close() // vai aguardar a aplicação fechar
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Javascript Gym',
+        latitude: -13.2248733,
+        longitude: -49.5076427,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -13.2248733,
+        longitude: -49.5076427,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})

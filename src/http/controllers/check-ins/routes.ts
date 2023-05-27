7af650b51c23bd4f23460ab70a4358fc9dd2345a
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-05-27 10:05:59 -0300

    fix: corigindo parametro da requisição.

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -13,7 +13,7 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
 
-  app.post('/gyms/:gymsId/check-ins', create)
+  app.post('/gyms/:gymId/check-ins', create)
 
   app.patch('/check-ins/:checkInId/validate', validate)
 }

690f3a783c55789bbeb4ba9b25da3718656b1ea3
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-05-28 11:02:51 -0300

    feat: colocando a role no token é o refreshToken

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -3,8 +3,10 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify({ onlyCookie: true }) // vai validar se o usuário esta autenticado, mas não vai olhar a informação que esta no cabeçalho da aplicação, no Authorization Bearer token
 
+  const { role } = request.user
+
   const token = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
@@ -13,7 +15,7 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   )
 
   const refreshToken = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,

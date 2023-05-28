fbe32e21c537ca632f470e204d8ff4cc019fdc7f
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-05-28 10:02:08 -0300

    feat: criado rota para fazer o refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,12 +4,15 @@ import { FastifyInstance } from 'fastify'
 
 import { authenticate } from './authenticate-controller'
 import { profile } from './profile'
+import { refresh } from './refresh'
 import { register } from './register'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   // Authenticated
   app.get('/me', { onRequest: [verifyJWT] }, profile)
